96acd4d6b2e4f030d97feced8f85adce2d34fa4b
greg-chuchro <51308516+greg-chuchro@users.noreply.github.com>
2021-06-11 01:39:15 +0900

    #5 switch branch before callback (#6)

--- action.sh
+++ action.sh
@@ -6,10 +6,13 @@ GIT_USER_EMAIL=$(git config --global user.email)
 set -e
 git config --global user.name "seqflow-action"
 git config --global user.email ""
-MINOR_BRANCHES=("origin/main" $(git branch --remotes --list origin/v[0-9]*))
+MINOR_BRANCHES=($(git branch --remotes --list origin/v[0-9]*))
 for BRANCH_FULL_NAME in "${MINOR_BRANCHES[@]}"; do
     BRANCH_NAME=${BRANCH_FULL_NAME#"origin/"}
+    git switch --orphan $BRANCH_NAME
+    git pull --depth=1 origin $BRANCH_NAME
     eval "$SEQFLOW_CALLBACK"
 done
+git switch main
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
