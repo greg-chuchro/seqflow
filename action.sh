efbef6c745dd5e51255ba0d98092bf4e793ff98c
greg-chuchro <gchuchro@acm.org>
2021-06-11 00:40:30 +0900

    #3 rename callback variable

--- action.sh
+++ action.sh
@@ -9,7 +9,7 @@ git config --global user.email ""
 MINOR_BRANCHES=("origin/main" $(git branch --remotes --list origin/v[0-9]*))
 for BRANCH_FULL_NAME in "${MINOR_BRANCHES[@]}"; do
     BRANCH_NAME=${BRANCH_FULL_NAME#"origin/"}
-    eval "$CALLBACK"
+    eval "$SEQFLOW_CALLBACK"
 done
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
