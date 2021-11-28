066ddf231e8fd8833ebdb9e6a24433af78e4af00
greg-chuchro <gchuchro@acm.org>
2021-11-29 00:15:36 +0900

    #7 main branch name (#8)

--- action.sh
+++ action.sh
@@ -1,11 +1,13 @@
-git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-git fetch
 set +e
 GIT_USER_NAME=$(git config --global user.name)
 GIT_USER_EMAIL=$(git config --global user.email)
 set -e
 git config --global user.name "seqflow-action"
 git config --global user.email ""
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+git fetch
+MAIN_BRANCH_NAME=$(git branch --show-current)
+
 MINOR_BRANCHES=($(git branch --remotes --list origin/v[0-9]*))
 for BRANCH_FULL_NAME in "${MINOR_BRANCHES[@]}"; do
     BRANCH_NAME=${BRANCH_FULL_NAME#"origin/"}
@@ -13,6 +15,7 @@ for BRANCH_FULL_NAME in "${MINOR_BRANCHES[@]}"; do
     git pull --depth=1 origin $BRANCH_NAME
     eval "$SEQFLOW_CALLBACK"
 done
-git switch main
+
+git switch $MAIN_BRANCH_NAME
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
